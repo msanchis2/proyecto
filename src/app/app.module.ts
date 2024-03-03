4612785386c7aef6653ee8e727bc71a09e02afb8
msanchis2 <martisanchis2000@gmail.com>
2024-03-03 10:27:43 +0100

    variables scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing-module';
+import { AppRoutingModule } from './app-routing.module';
 import { CardComponent } from "./shared/card/card.component";
 
 @NgModule({
